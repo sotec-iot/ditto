9281cfd315bbcd83f36fd1f51a48c3053db67a4d
Dominik Basner <d.basner@sotec.eu>
2024-03-11 13:46:33 +0100

    unmodule

---
+++ connectivity/service/src/main/java/org/eclipse/ditto/connectivity/service/messaging/googlepubsub/GooglePubSubPublishTarget.java
@@ -0,0 +1,9 @@
+package org.eclipse.ditto.connectivity.service.messaging.googlepubsub;
+
+import org.eclipse.ditto.connectivity.service.messaging.PublishTarget;
+
+/**
+ * A Google PubSub target (topic) to which messages can be published.
+ */
+public class GooglePubSubPublishTarget implements PublishTarget {
+}
